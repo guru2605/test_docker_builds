7eeb499049031c52c8adcab61ab95365a113464e
Gurudatta Shanbhag <gurushan13@gmail.com>
2021-06-19 13:21:38 +0530

    #added check2

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/', methods=["GET"])
 def index():
     return "Running CST FLASK DOCKER..."
+
+@app.route('/check2', methods=["GET"])
+def index():
+    return "Running Check2..."
